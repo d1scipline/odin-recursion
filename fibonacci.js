64d035d733465e0f3e5b3d5ab6825f68d8ffdd79
d1scipline <143661704+d1scipline@users.noreply.github.com>
2026-02-18 07:55:58 +0300

    add recursive solution to fibonacci

--- fibonacci.js
+++ fibonacci.js
@@ -8,3 +8,19 @@ function fibonacciIterative(n) {
   }
   return sequence;
 }
+
+function fibsRec(n) {
+  let sequence = [];
+  if (n <= 1) {
+    return [0];
+  }
+  if (n == 2) {
+    return [0, 1];
+  }
+  sequence = fibsRec(n - 1);
+  sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
+  return sequence;
+}
+
+console.log(fibonacciIterative(8));
+console.log(fibsRec(8));
